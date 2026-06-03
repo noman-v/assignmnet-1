06d7a321fbfd1810446dc179703ae841c7ea45f8
Vaibhav Sharma <viburasgotra2007@gmail.com>
2026-06-04 00:11:13 +0530

    percentage calculator

---
+++ src/d1p9.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class d1p9 {
+    public static void main(String[] args){
+        int a,b,c,d,e;
+        System.out.print("Enter the marks of English :");
+        Scanner input = new Scanner(System.in);
+        a=input.nextInt();
+        System.out.print("Enter the marks of Maths :");
+        b=input.nextInt();
+        System.out.print("Enter the marks of Science :");
+        c=input.nextInt();
+        System.out.print("Enter the marks of Social Science :");
+        d=input.nextInt();
+        System.out.print("Enter the marks of Computer Hindi :");
+        e=input.nextInt();
+        float k=(((a+b+c+d+e)*100)/500.0f);
+        System.out.println("\n\n--your overall percentage is : "+k);
+        if(k>=40.0f){
+            System.out.print(" , so you pass");
+        }
+        else{
+            System.out.print(" , sorry better luck next time");
+        }
+    }
+}
