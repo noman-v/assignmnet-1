bb3144cb7b3946f245a42c489295c216c0dcd1c2
Vaibhav Sharma <viburasgotra2007@gmail.com>
2026-06-03 22:51:01 +0530

    4

--- src/d1p2.java
+++ src/d1p2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class d2p2 {
+public class d1p2 {
     public static void main(String[] args) {
         Scanner input =  new Scanner(System.in);
         System.out.print("Enter the number : ");
---
+++ src/d1p4_swap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class d1p4_swap {
+    public static void main(String[] args) {
+        int a,b,c;
+        System.out.print("Enter the first number(a)");
+        Scanner input = new Scanner(System.in);
+        a=input.nextInt();
+        System.out.print("Enter the second number(b)");
+        b=input.nextInt();
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
