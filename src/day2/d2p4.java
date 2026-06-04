4bfb36cd83fbb323d672065d691e1165b8248a6d
Vaibhav Sharma <viburasgotra2007@gmail.com>
2026-06-04 12:54:00 +0530

    checking for pos(+),neg(-) or zero

---
+++ src/day2/d2p4.java
@@ -0,0 +1,21 @@
+package day2;
+import java.util.Scanner;
+public class d2p4 {
+public static void main(String[] args) {
+    Scanner input =new Scanner(System.in);
+    System.out.print("Enter the number to be checked :");
+    int n=input.nextInt();
+    if(n<0) {System.out.println("Negative number");}
+
+    else if(n==0){
+        System.out.println("Zero");
+    }
+    else{
+
+        System.out.println("Positive");}
+    }
+
+
+
+}
+
